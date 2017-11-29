9b84c598f44354e99a651ec62c63a7fc7672455b
Romanna Koval <rkoval@evolutiongaming.com>
2017-11-29 16:15:50 +0100

    Add tests for createCalendar()

--- src/test/scala/com/svitovyda/booking/RequestsProcessorSpec.scala
+++ src/test/scala/com/svitovyda/booking/RequestsProcessorSpec.scala
@@ -5,11 +5,11 @@ import java.time.LocalTime
 import com.svitovyda.booking.Calendar.{Period, TimeRange}
 import org.scalatest.{MustMatchers, WordSpecLike}
 
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Right, Success}
 
 
 class RequestsProcessorSpec extends WordSpecLike with MustMatchers {
-  "parseHeader" must {
+  "parseHeader()" must {
     "correctly process valid working hours" in {
       RequestsProcessor.parseHeader("1200 1315") must be(Success(TimeRange(
         LocalTime.of(12, 0), LocalTime.of(13, 15))))
@@ -23,6 +23,33 @@ class RequestsProcessorSpec extends WordSpecLike with MustMatchers {
       RequestsProcessor.parseHeader("0966 2356") must be (a[Failure[TimeRange]])
       RequestsProcessor.parseHeader("ab45 2356") must be (a[Failure[TimeRange]])
       RequestsProcessor.parseHeader("09:45 2356") must be (a[Failure[TimeRange]])
+    }
+  }
+
+  "createCalendar()" must {
+    "correctly create a calendar" in {
+      RequestsProcessor.createCalendar("0945 1830", List()) must be (Right[Error, Calendar](
+        Calendar(TimeRange(LocalTime.of(9, 45), LocalTime.of(18, 30)))))
+
+      RequestsProcessor.createCalendar(
+        "0945 1830",
+        List(
+          "2015-08-17 10:17:06 EMP001",
+          "2015-08-21 09:00 2",
+          "2015-08-16 12:34:56 EMP002",
+          "2015-08-21 09:00 2",
+          "2015-08-16 09:28:23 EMP003",
+          "2015-08-22 14:00 2",
+          "2015-08-17 11:23:45 EMP004",
+          "2015-08-22 16:00 1",
+          "2015-08-15 17:29:12 EMP005",
+          "2015-08-21 16:00 3"
+        )
+      ) must be (a[Right[Error, Calendar]])
+    }
+
+    "reject creating calendar" in {
+      RequestsProcessor.createCalendar("2145 1830", List()) must be (a[Left[Error, Calendar]])
     }
   }
 }
